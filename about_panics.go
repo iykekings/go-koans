0f04fa3f37a6fb5044f3badba2963341c540e052
Ikechukwu <iykekings36@gmail.com>
2019-10-20 12:29:40 +0100

    solve panic

--- about_panics.go
+++ about_panics.go
@@ -4,11 +4,17 @@ func divideFourBy(i int) int {
 	return 4 / i
 }
 
-const __divisor__ = 0
+const divisor = 0
 
 func aboutPanics() {
-	assert(__delete_me__) // panics are exceptional errors at runtime
+	panicRecover := func() {
+		if r := recover(); r != nil {
+			println("recovered")
+		}
+	}
+	defer panicRecover()
+	assert(true) // panics are exceptional errors at runtime
 
-	n := divideFourBy(__divisor__)
+	n := divideFourBy(divisor)
 	assert(n == 2) // panics are exceptional errors at runtime
 }
